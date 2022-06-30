4c86497f36fdfc938cee3b8d8ea9621bf5e0a45e
abhiedward001 <iam001abhi@gmail.com>
2022-06-30 13:45:34 +0530

    added about component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import Input from './components/Input'
-
+import About from './components/About';
 function App() {
   return (
     <>
     
       <Navbar Title={"Text_Switcher"} ></Navbar>
       <Input Heading={"Enter Your Text Below :-"}> </Input>
+      <About></About>
     </>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,29 +3,54 @@ import React, { useState } from 'react'
 export default function Input(props) {
 
     //creating a useState hook through which we will update the input text
-    const [Text, setText] = useState("Hello I am dummy text");
-    
+    const [Text, setText] = useState("");
+    const [totalwords, setTotalwords] = useState(Text.length);
+
+
+
+
     //function which is  handling the update of text
     const updateTextupper = () => {
         let uppertext = Text.toUpperCase();
         setText(uppertext);
     }
+    const updateTextlower = () => {
+        let lowertext = Text.toLowerCase();
+        setText(lowertext);
+    }
 
     //Function which is updating the onchange text
     const handleonChange = (event) => {
         setText(event.target.value);
+
+        let count = 0;
+        let dummy_txt=Text + " ";
+        for (let i = 0; i < dummy_txt.length; i++) {
+            if (dummy_txt[i] === " " && dummy_txt[i + 1] !== " ") count++;
+        }
+       
+        setTotalwords(count);
+        console.log(count);
     }
+
     return (
 
         <>
-            <div className="container">
+            <div className="container my-5" >
                 <div className="mb-5">
                     <h1>{props.Heading}</h1>
                     <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={Text} onChange={handleonChange}></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={updateTextupper}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={updateTextupper}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={updateTextlower}>Convert to Lowercase</button>
             </div>
-
+            <div className="container my-3">
+                <h1>Your text summary is here</h1>
+                <p>{totalwords} Words and {Text.length} Charachters</p>
+                <p>{0.008*totalwords} Mintues Read</p>
+                <h2>Preview</h2>
+                <p>{Text}</p>
+            </div>
         </>
 
     )
